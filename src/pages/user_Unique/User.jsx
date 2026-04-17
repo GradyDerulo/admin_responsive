de19f12f709c475ba4e8aa1b3e8bb514c6bd4c12
DERULO <derumanga@gmail.com>
2026-04-17 03:43:55 -0700

    commit 5ieme

--- src/pages/user_Unique/User.jsx
+++ src/pages/user_Unique/User.jsx
@@ -5,13 +5,13 @@ import "./user.scss";
 
   export const singleUser = {
     id: 1,
-    title: "John Doe",
-    img: "/use6.jpg",
+    title: "Grady ",
+    img: "/profile.jpg",
     info: {
-      username: "Johndoe99",
-      fullname: "John Doe",
-      email: "johndoe@gmail.com",
-      phone: "123 456 789",
+      username: "Grady Manga",
+      fullname: "Grady Manga",
+      email: "derumanga@gmail.com",
+      phone: "+243 854 969 505",
       status: "verified",
     },
     chart: {
@@ -59,27 +59,27 @@ import "./user.scss";
     },
     activities: [
       {
-        text: "John Doe purchased Playstation 5 Digital Edition",
+        text: "Grady Manga purchased Playstation 5 Digital Edition",
         time: "3 day ago",
       },
       {
-        text: "John Doe added 3 items into their wishlist",
+        text: "Grady Manga added 3 items into their wishlist",
         time: "1 week ago",
       },
       {
-        text: "John Doe purchased Sony Bravia KD-32w800",
+        text: "Grady Manga purchased Sony Bravia KD-32w800",
         time: "2 weeks ago",
       },
       {
-        text: "John Doe reviewed a product",
+        text: "Grady Manga reviewed a product",
         time: "1 month ago",
       },
       {
-        text: "John Doe added 1 items into their wishlist",
+        text: "Grady Manga 1 items into their wishlist",
         time: "1 month ago",
       },
       {
-        text: "John Doe reviewed a product",
+        text: "Grady Manga reviewed a product",
         time: "2 months ago",
       },
     ],
@@ -88,90 +88,6 @@ import "./user.scss";
 
 
 const User = ()=> {
-
-  /*   export const singleUser = {
-        id: 1,
-        title: "John Doe",
-        img: "/use6.jpg",
-        info: {
-          username: "Johndoe99",
-          fullname: "John Doe",
-          email: "johndoe@gmail.com",
-          phone: "123 456 789",
-          status: "verified",
-        },
-        chart: {
-          dataKeys: [
-            { name: "visits", color: "#82ca9d" },
-            { name: "clicks", color: "#8884d8" },
-          ],
-          data: [
-            {
-              name: "Sun",
-              visits: 4000,
-              clicks: 2400,
-            },
-            {
-              name: "Mon",
-              visits: 3000,
-              clicks: 1398,
-            },
-            {
-              name: "Tue",
-              visits: 2000,
-              clicks: 3800,
-            },
-            {
-              name: "Wed",
-              visits: 2780,
-              clicks: 3908,
-            },
-            {
-              name: "Thu",
-              visits: 1890,
-              clicks: 4800,
-            },
-            {
-              name: "Fri",
-              visits: 2390,
-              clicks: 3800,
-            },
-            {
-              name: "Sat",
-              visits: 3490,
-              clicks: 4300,
-            },
-          ],
-        },
-        activities: [
-          {
-            text: "John Doe purchased Playstation 5 Digital Edition",
-            time: "3 day ago",
-          },
-          {
-            text: "John Doe added 3 items into their wishlist",
-            time: "1 week ago",
-          },
-          {
-            text: "John Doe purchased Sony Bravia KD-32w800",
-            time: "2 weeks ago",
-          },
-          {
-            text: "John Doe reviewed a product",
-            time: "1 month ago",
-          },
-          {
-            text: "John Doe added 1 items into their wishlist",
-            time: "1 month ago",
-          },
-          {
-            text: "John Doe reviewed a product",
-            time: "2 months ago",
-          },
-        ],
-      };
-      
-     */
 
 
     return (
